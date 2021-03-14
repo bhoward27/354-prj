feec8c1789c1f5d9f3d0f4ed760d10fa64c57c89
Benjamin Howard <bentlife@icloud.com>
2021-03-14 16:24:51 -0700

    Create resultsLabelRow.
    Create resultsLabelRow, which is a frame that holds the labels Title, Author, and Number of Copies that will go above the search results. Light refactor by extracting methods.

--- py/ui/searchWindow.py
+++ py/ui/searchWindow.py
@@ -2,15 +2,39 @@ import tkinter as tk
 
 class SearchWindow:
     def __init__(self):
+        #   Set up mainWindow.
         self.mainWindow = tk.Tk()
         self.mainWindow.title("Search Library Catalogue")
 
+        #   Create frames.
         self.searchFrame = tk.Frame(self.mainWindow)
+        self.resultsFrame = tk.Frame(self.mainWindow)
+        self.resultsLabelRow = tk.Frame(self.resultsFrame)
         
+        #   Initialize search variables.
         self.searchOption = tk.IntVar()
         self.searchOption.set(0)
         self.searchEntry = tk.StringVar()
 
+        self.createSearchFrameWidgets()
+        self.packSearchFrameWidgets()
+
+        self.resultsTitleLabel = tk.Label(self.resultsLabelRow, text = "Title\t\t\t\t")
+        self.resultsAuthorLabel = tk.Label(self.resultsLabelRow, text = "Author\t\t\t")
+        self.resultsNumCopiesLabel = tk.Label(self.resultsLabelRow, text = "Number of Copies Available")
+        
+        self.resultsTitleLabel.pack(side = "left")
+        self.resultsAuthorLabel.pack(side = "left")
+        self.resultsNumCopiesLabel.pack(side = "left")
+
+        #   Pack frames.
+        self.searchFrame.pack()
+        self.resultsLabelRow.pack()
+        self.resultsFrame.pack()
+
+        tk.mainloop()
+
+    def createSearchFrameWidgets(self):
         self.searchLabel = tk.Label(self.searchFrame, text = "Search by")
         self.searchTitleRb = tk.Radiobutton(self.searchFrame, text = "Title", 
                                                 variable = self.searchOption, value = 0)
@@ -19,15 +43,12 @@ class SearchWindow:
         self.searchBox = tk.Entry(self.searchFrame, width = 10, textvariable = self.searchEntry)
         self.searchButton = tk.Button(self.searchFrame, text = "Search", command = self.search)
 
+    def packSearchFrameWidgets(self):
         self.searchLabel.pack(side = "left")
         self.searchTitleRb.pack(side = "left")
         self.searchAuthorRb.pack(side = "left")
         self.searchBox.pack(side = "left")
         self.searchButton.pack(side = "left")
-
-        self.searchFrame.pack()
-
-        tk.mainloop()
 
     def search(self):
         print(str(self.searchEntry))
