80f414bb0278135a6290ed5447074950c7ea98ea
Aidan Tyndall <>
2021-03-28 16:30:05 -0700

    Add registration page.

--- flaskapp.py
+++ flaskapp.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask (__name__)
 
+app.config['SECRET_KEY'] = 'c14c7141e792cbae4abfc4ddbbd9b048'
+
 @app.route('/')
-def main():
-    return render_template('main.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.fName.data} {form.lName.data}!', 
+            'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
