2c14299c7d7ec2eb4f8278d74b7dbde1decd496c
Aidan Tyndall <>
2021-03-30 14:17:30 -0700

    Update formatting. Center page content.

--- flaskapp.py
+++ flaskapp.py
@@ -1,26 +1,30 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-app = Flask (__name__)
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'c14c7141e792cbae4abfc4ddbbd9b048'
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.fName.data} {form.lName.data}!', 
-            'success')
+        flash(f'Account created for {form.fName.data} {form.lName.data}!',
+              'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route('/login')
 def login():
     form = LoginForm()
     return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
