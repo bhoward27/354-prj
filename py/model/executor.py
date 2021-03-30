e2c4e204b2b0648cbe73092d7dfd637f2d499855
Benjamin Howard <bentlife@icloud.com>
2021-03-29 18:24:36 -0700

    Add private __insert method to Executor class.
    Add private __insert method, which inserts a single record. Seems to work. Fix various things in Executor. Verify that __setValidTableNames() and isValidTableName() work.

--- py/model/executor.py
+++ py/model/executor.py
@@ -4,12 +4,11 @@ from os import chdir
 
 class Executor:
     def __init__(self):
-        self.db = connectToDatabase()
+        self.db = self.connectToDatabase()
         self.cursor = self.db.cursor()
-        __setValidTableNames()
+        self.__setValidTableNames()
 
     def __del__(self):
-        print("Running Cursor.__del__()...")
         self.cursor.close()
         self.db.disconnect()
     
@@ -19,20 +18,41 @@ class Executor:
             host = "localhost",
             user = "root",
             passwd = "Fattest5!",
-            # database = "test_project",
+            database = "test_project",
         )
     
     def __setValidTableNames(self):
         self.validTableNames = []
-        db.execute("SHOW TABLES;")
-        results = db.fetchall()
+        self.cursor.execute("SHOW TABLES;")
+        results = self.cursor.fetchall()
         for result in results:
-            self.validTableNames.append(result)
+            self.validTableNames.append(result[0].lower())
         print("Valid table names: ", self.validTableNames)
 
     def insert(self, tableName, values):
-        if not isValidTableName(tableName):
+        if not self.isValidTableName(tableName):
+            print("ValueError: tableName '" + tableName + "' is invalid.")
             raise ValueError
         
+        if (isinstance(values, tuple) or isinstance(values, list)) \
+            and any(isinstance(x, tuple) for x in values):
+            for record in values:
+                self.__insert(tableName, record)
+            self.db.commit()
+        elif isinstance(values, tuple):
+            self.__insert(tableName, values)
+            self.db.commit()
+        else:
+            print("TypeError: values must be a tuple, list of tuples, or tuple of tuples.")
+            raise TypeError
+
+    def __insert(self, tableName, record):
+        sql = "INSERT INTO " + tableName + " VALUES ("
+        numColumns = len(record)
+        for i in range(1, numColumns + 1):
+            sql += "%s, "
+        sql = sql[: len(sql) - 2] + ");"
+        self.cursor.execute(sql, record)
+
     def isValidTableName(self, tableName):
         return tableName.lower() in self.validTableNames
