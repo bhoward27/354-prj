b4b2868bbf01a2aa97d31b5ee4d0632d686268ee
Aidan Tyndall <>
2021-04-12 20:07:34 -0700

    Register form now adds user to sql database

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from mysql import connector
+
+from py.model.executor import Executor
+exec = Executor()
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'c14c7141e792cbae4abfc4ddbbd9b048'
@@ -14,9 +19,28 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash('Account created for %s %s!' % (form.fName.data, form.lName.data),
-              'success')
+        # TODO: run this in executor.py
+        sql = "SELECT MAX(lib_card_num) from Member"
+        exec.cursor.execute(sql)
+        for row in exec.cursor:
+            print(row)
+            lib_card_num = int(row[0])+1
+            print(lib_card_num)
+
+        # Check for NULL values and add default values
+        if not form.mName.data:
+            form.mName.data = None
+        status, fines = 1, 0
+        
+        exec.insert('Member', (str(lib_card_num), form.address.data,
+                    form.email.data, form.password.data, status, fines,
+                    form.fName.data, form.mName.data, form.lName.data))
+
+        flash('Account created for %s %s!' %
+              (form.fName.data, form.lName.data), 'success')
+
         return redirect(url_for('home'))
+
     return render_template('register.html', title='Register', form=form)
 
 
